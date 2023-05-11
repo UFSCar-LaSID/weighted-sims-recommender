07bf3d2b2c0e5ef269d59984c7b84b337ee9f4b1
GregorioFornetti <63006135+GregorioFornetti@users.noreply.github.com>
2023-05-11 12:29:26 -0300

    Jester

--- scripts/recommenders/itemSim.py
+++ scripts/recommenders/itemSim.py
@@ -86,17 +86,15 @@ class ItemSim(object):
     
     def fit_sem_tempo(self, df_train):
         n_items = self.embeddings.shape[0]
-        print(self.embeddings.shape)
         items_per_batch = int(kw.MEM_SIZE_LIMIT / (8 * n_items))
         nearest_neighbors = np.empty((n_items, self.k))
         nearest_sims = np.empty((n_items, self.k))
         embeddings_norm = self.embeddings / np.sqrt(np.sum(self.embeddings**2, axis=1)).reshape(-1,1) # Normaliza embeddings
         for i in range(0, n_items, items_per_batch):
             batch_sims = np.dot(embeddings_norm[i:i+items_per_batch], embeddings_norm.T) # Calcula distancia
-            print(batch_sims.shape)
             np.fill_diagonal(batch_sims[:, i:i+items_per_batch], -np.inf)
-            nearest_neighbors[i:i+items_per_batch] = np.argpartition(-batch_sims, kth=min(self.k-1, batch_sims.shape[0]), axis=1)[:, :self.k] # captura k mais similares
-            nearest_sims[i:i+items_per_batch] = -np.partition(-batch_sims, kth=min(self.k-1, batch_sims.shape[0]), axis=1)[:, :self.k] # captura similaridades dos k vizinhos
+            nearest_neighbors[i:i+items_per_batch] = np.argpartition(-batch_sims, kth=min(self.k-1, batch_sims.shape[0]-1), axis=1)[:, :self.k] # captura k mais similares
+            nearest_sims[i:i+items_per_batch] = -np.partition(-batch_sims, kth=min(self.k-1, batch_sims.shape[0]-1), axis=1)[:, :self.k] # captura similaridades dos k vizinhos
         sim_table = tc.SFrame({
             'id_item': self.sparse_repr.get_item_id(np.repeat(np.arange(n_items), self.k).astype(int)),
             'similar': self.sparse_repr.get_item_id(nearest_neighbors.flatten().astype(int)),
