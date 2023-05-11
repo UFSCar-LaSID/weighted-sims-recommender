6831a65ab7baa673dd5ffd35681c5203c0a3ca18
GregorioFornetti <63006135+GregorioFornetti@users.noreply.github.com>
2023-05-11 00:57:40 -0300

    fits itemSim

--- scripts/recommenders/itemSim.py
+++ scripts/recommenders/itemSim.py
@@ -20,7 +20,13 @@ class ItemSim(object):
         return df
 
 
-    def fit(self, df_train):
+    def fit(self, df_train, tempo=False):
+        if tempo:
+            self.fit_com_tempo(df_train)
+        else:
+            self.fit_sem_tempo(df_train)
+    
+    def fit_com_tempo(self, df_train):
         inicio = datetime.now()
         n_items = self.embeddings.shape[0]
         items_per_batch = int(kw.MEM_SIZE_LIMIT / (8 * n_items))
@@ -77,6 +83,35 @@ class ItemSim(object):
         print(f'Tempo que levou para criar o modelo: {new_now - old_now}')
         fim = datetime.now()
         print(f'Tempo total: {fim - inicio}')
+    
+    def fit_sem_tempo(self, df_train):
+        n_items = self.embeddings.shape[0]
+        items_per_batch = int(kw.MEM_SIZE_LIMIT / (8 * n_items))
+        nearest_neighbors = np.empty((n_items, self.k))
+        nearest_sims = np.empty((n_items, self.k))
+        embeddings_norm = self.embeddings / np.sqrt(np.sum(self.embeddings**2, axis=1)).reshape(-1,1) # Normaliza embeddings
+        for i in range(0, n_items, items_per_batch):
+            batch_sims = np.dot(embeddings_norm[i:i+items_per_batch], embeddings_norm.T) # Calcula distancia
+            np.fill_diagonal(batch_sims[:, i:i+items_per_batch], -np.inf)
+            nearest_neighbors[i:i+items_per_batch] = np.argpartition(-batch_sims, kth=self.k-1, axis=1)[:, :self.k] # captura k mais similares
+            nearest_sims[i:i+items_per_batch] = -np.partition(-batch_sims, kth=self.k-1, axis=1)[:, :self.k] # captura similaridades dos k vizinhos
+        sim_table = tc.SFrame({
+            'id_item': self.sparse_repr.get_item_id(np.repeat(np.arange(n_items), self.k).astype(int)),
+            'similar': self.sparse_repr.get_item_id(nearest_neighbors.flatten().astype(int)),
+            'score': nearest_sims.flatten()
+        })
+        self.sims = sim_table
+        sframe = tc.SFrame(df_train[[kw.COLUMN_USER_ID, kw.COLUMN_ITEM_ID]])
+        self.model = tc.recommender.item_similarity_recommender.create(
+            observation_data=sframe,
+            user_id=kw.COLUMN_USER_ID,
+            item_id=kw.COLUMN_ITEM_ID,
+            similarity_type='cosine',
+            only_top_k=self.k,
+            nearest_items=sim_table,
+            target_memory_usage=kw.MEM_SIZE_LIMIT,
+            verbose=False
+        )
     
     def recommend(self, df_test):
         recommendations = self.model.recommend(
