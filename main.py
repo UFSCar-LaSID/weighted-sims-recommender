a42e2085a4f644c394694b9aa54943cbfa33373c
GregorioFornetti <63006135+GregorioFornetti@users.noreply.github.com>
2023-05-11 00:58:34 -0300

    Update main.py

--- main.py
+++ main.py
@@ -7,9 +7,8 @@ from scripts.file_handlers import get_embeddings_filepath, log_recommendations, 
 from scripts.recommenders import get_recommenders
 from scripts.recsys import remove_single_interactions, remove_cold_start
 
-DATASETS = ['Anime Recommendations', 'BestBuy', 'Book-Crossing', 'CiaoDVD',
-            'DeliciousBookmarks', 'Filmtrust', 'Jester', 'Last.FM - Listened',
-            'LibimSeTi', 'MovieLens', 'NetflixPrize', 'RetailRocket-Transactions'] # Mudar bases de dados aqui
+DATASETS = ['CiaoDVD', 'DeliciousBookmarks', 'Filmtrust', 'Jester', 'Last.FM - Listened',
+            'LibimSeTi', 'MovieLens', 'NetflixPrize', 'RetailRocket-Transactions', 'Book-Crossing'] # Mudar bases de dados aqui
 RECOMMENDERS = ['ALS_itemSim', 'BPR_itemSim'] # Mudar recomendadores aqui
 
 for dataset in get_datasets(datasets=DATASETS):
