23bb7cbc35857a21ef1b49d5a6e4f8687449c1ac
GregorioFornetti <63006135+GregorioFornetti@users.noreply.github.com>
2024-09-08 15:24:31 -0300

    Update retailrocket.py

--- scripts/preprocessing/retailrocket.py
+++ scripts/preprocessing/retailrocket.py
@@ -10,6 +10,12 @@ def preprocess_retailrocket():
     # Pasta de dataset
     DATASET_FOLDER = 'retailrocket'
 
+    # Nomes das colunas
+    COLUMN_INTERACTION_TYPE = 'type'
+    COLUMN_TRANSACTION_ID = 'id_transaction'
+    COLUMN_DATETIME = 'datetime'
+    COLUMN_TIMESTAMP = 'timestamp'
+
     # Define pasta de leitura
     intput_dir = os.path.join(kw.RAW_FOLDER, DATASET_FOLDER)
 
@@ -18,7 +24,7 @@ def preprocess_retailrocket():
     df_interactions = pd.read_csv(os.path.join(intput_dir, 'events.csv'), sep=',', header=0, index_col=False)
 
     # Arruma nome das colunas
-    df_interactions.columns = ['wrong_timestamp', COLUMN_USER_ID, COLUMN_INTERACTION_TYPE, COLUMN_ITEM_ID, COLUMN_TRANSACTION_ID]
+    df_interactions.columns = ['wrong_timestamp', kw.COLUMN_USER_ID, COLUMN_INTERACTION_TYPE, kw.COLUMN_ITEM_ID, COLUMN_TRANSACTION_ID]
 
     # Padroniza timestamps de itens da mesma interação
     last_timestamps = df_interactions.groupby(COLUMN_TRANSACTION_ID)['wrong_timestamp'].max()
@@ -33,8 +39,8 @@ def preprocess_retailrocket():
     df_interactions[COLUMN_DATETIME] = df_interactions[COLUMN_TIMESTAMP].apply(lambda x: str(datetime.fromtimestamp(x)).split('.')[0])
 
     # Reordena colunas
-    df_interactions = df_interactions[[COLUMN_USER_ID, COLUMN_ITEM_ID, COLUMN_INTERACTION_TYPE, COLUMN_TRANSACTION_ID, COLUMN_TIMESTAMP, COLUMN_DATETIME]]
-    df_interactions = df_interactions.sort_values([COLUMN_TIMESTAMP, COLUMN_USER_ID, COLUMN_ITEM_ID])
+    df_interactions = df_interactions[[kw.COLUMN_USER_ID, kw.COLUMN_ITEM_ID, COLUMN_INTERACTION_TYPE, COLUMN_TRANSACTION_ID, COLUMN_TIMESTAMP, COLUMN_DATETIME]]
+    df_interactions = df_interactions.sort_values([COLUMN_TIMESTAMP, kw.COLUMN_USER_ID, kw.COLUMN_ITEM_ID])
 
 
     # =========================================== ITENS ============================================
@@ -46,7 +52,7 @@ def preprocess_retailrocket():
     df_items = pd.concat([df_items_1, df_items_2], axis=0)
 
     # Arruma nome das colunas
-    df_items.columns = [COLUMN_TIMESTAMP, COLUMN_ITEM_ID, 'property', 'value']
+    df_items.columns = [COLUMN_TIMESTAMP, kw.COLUMN_ITEM_ID, 'property', 'value']
 
     # Modifica o timestamp para segundos ao invés de milisegundos (padrão das outras bases de dados)
     df_items[COLUMN_TIMESTAMP] = df_items[COLUMN_TIMESTAMP] // 1000
@@ -55,18 +61,18 @@ def preprocess_retailrocket():
     df_items[COLUMN_DATETIME] = df_items[COLUMN_TIMESTAMP].apply(lambda x: str(datetime.fromtimestamp(x)).split('.')[0])
 
     # Reordena colunas
-    df_items = df_items[[COLUMN_ITEM_ID, 'property', 'value', COLUMN_TIMESTAMP, COLUMN_DATETIME]]
-    df_items = df_items.sort_values([COLUMN_TIMESTAMP, COLUMN_ITEM_ID])
+    df_items = df_items[[kw.COLUMN_ITEM_ID, 'property', 'value', COLUMN_TIMESTAMP, COLUMN_DATETIME]]
+    df_items = df_items.sort_values([COLUMN_TIMESTAMP, kw.COLUMN_ITEM_ID])
 
 
     # =========================================== SALVAR ===========================================
     # Cria pasta de saída
-    output_dir = os.path.join(PROCESSED_FOLDER, DATASET_FOLDER)
+    output_dir = os.path.join(kw.PREPROCESSED_DATASET_FOLDER, DATASET_FOLDER)
     os.makedirs(output_dir, exist_ok=True)
 
     # Salva bases
-    df_items.to_csv(os.path.join(output_dir, FILE_ITEMS), sep=DELIMITER, encoding=ENCODING, quoting=QUOTING, quotechar=QUOTECHAR, header=True, index=False)
-    df_interactions.to_csv(os.path.join(output_dir, FILE_INTERACTIONS), sep=DELIMITER, encoding=ENCODING, quoting=QUOTING, quotechar=QUOTECHAR, header=True, index=False)
+    df_items.to_csv(os.path.join(output_dir, kw.FILE_ITEMS), sep=kw.DELIMITER, encoding=kw.ENCODING, quoting=kw.QUOTING, quotechar=kw.QUOTECHAR, header=True, index=False)
+    df_interactions.to_csv(os.path.join(output_dir, kw.FILE_INTERACTIONS), sep=kw.DELIMITER, encoding=kw.ENCODING, quoting=kw.QUOTING, quotechar=kw.QUOTECHAR, header=True, index=False)
 
 
     # ============================ GERAR ARQUIVOS APENAS COM TRANSACOES ============================
@@ -75,10 +81,10 @@ def preprocess_retailrocket():
 
     # =========================================== SALVAR ===========================================
     # Cria pasta de saída
-    output_dir = os.path.join(PROCESSED_FOLDER, '{}-transactions'.format(DATASET_FOLDER))
+    output_dir = os.path.join(kw.PREPROCESSED_DATASET_FOLDER, '{}-transactions'.format(DATASET_FOLDER))
     os.makedirs(output_dir, exist_ok=True)
 
     # Salva bases
-    df_items.to_csv(os.path.join(output_dir, FILE_ITEMS), sep=DELIMITER, encoding=ENCODING, quoting=QUOTING, quotechar=QUOTECHAR, header=True, index=False)
-    df_interactions.to_csv(os.path.join(output_dir, FILE_INTERACTIONS), sep=DELIMITER, encoding=ENCODING, quoting=QUOTING, quotechar=QUOTECHAR, header=True, index=False)
+    df_items.to_csv(os.path.join(output_dir, kw.FILE_ITEMS), sep=kw.DELIMITER, encoding=kw.ENCODING, quoting=kw.QUOTING, quotechar=kw.QUOTECHAR, header=True, index=False)
+    df_interactions.to_csv(os.path.join(output_dir, kw.FILE_INTERACTIONS), sep=kw.DELIMITER, encoding=kw.ENCODING, quoting=kw.QUOTING, quotechar=kw.QUOTECHAR, header=True, index=False)
     print('OK!')
