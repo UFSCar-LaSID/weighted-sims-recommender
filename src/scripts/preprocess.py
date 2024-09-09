fe8b69de3fddd426f0bc86f776e13618335e8b3d
GregorioFornetti <63006135+GregorioFornetti@users.noreply.github.com>
2024-09-09 00:12:39 -0300

    Update preprocess.py

--- src/scripts/preprocess.py
+++ src/scripts/preprocess.py
@@ -9,7 +9,7 @@ import argparse
 import pandas as pd
 import src as kw
 from src.preprocessing.retailrocket import preprocess_retailrocket
-from src.parameters_handle import ask_options
+from src.parameters_handle import get_input
 
 
 
@@ -28,36 +28,14 @@ PREPROCESS_TABLE = pd.DataFrame(
 
 # ----- Collect dataset options from command line or interactive menu -----
 
-parser = argparse.ArgumentParser(description='Framework de avaliação 3D genérico.',
-                                     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-parser.add_argument('--datasets', type=str, default=None, help='Dataset names (or indexes) to preprocess. If not provided, a interactive menu will be shown. If "all" is provided, all datasets will be preprocessed.')
-
-args = parser.parse_args()
-
-dataset_option = args.datasets
-
-if dataset_option is None:
-    options = ask_options('dataset', PREPROCESS_TABLE, kw.DATASET_NAME)
-elif dataset_option == 'all':
-    options = PREPROCESS_TABLE.index.tolist()
-elif dataset_option.replace(" ", "").isdigit():
-    options = []
-    dataset_option = dataset_option.split(' ')
-    for option in dataset_option:
-        if int(option) not in PREPROCESS_TABLE.index:
-            raise ValueError('Dataset index {} not found!'.format(option))
-        options.append(int(option))
-else:
-    options = []
-    options_names = dataset_option.split(' ')
-    for option_name in options_names:
-        if not PREPROCESS_TABLE[kw.DATASET_NAME].str.contains(option_name).any():
-            raise ValueError('Dataset {} not found!'.format(option_name))
-        options.append(PREPROCESS_TABLE[PREPROCESS_TABLE[kw.DATASET_NAME].str.contains(option_name)].index.tolist()[0])
-
-options = list(set(options))
-
-
+options = get_input('Choose datasets to preprocess', [
+    {
+        'name': 'datasets',
+        'description': 'Dataset names (or indexes) to preprocess. If not provided, a interactive menu will be shown. If "all" is provided, all datasets will be preprocessed.',
+        'options': PREPROCESS_TABLE,
+        'name_column': kw.DATASET_NAME
+    }
+])[0]
 
 
 # ----- Preprocess datasets -----
